d87a6fcb58f1703656511810cec3cb6c067e581f
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-06 02:16:38 +0500

    Audio and Video fixed

--- type/audio.py
+++ type/audio.py
@@ -9,12 +9,17 @@ def _audio():
     def __audio(message):
         bot.reply_to(message, "<b>Audio Received</b>", parse_mode='HTML')
         bot.send_chat_action(message.chat.id, 'upload_audio')
-
         fileID = message.audio.file_id
         file_info = bot.get_file(fileID)
         downloaded_file = bot.download_file(file_info.file_path)
         content = f"<b>{random.choice(PERSON)} {random.choice(CONTENT)}.</b>"
-        bot.send_audio(CONFESSION, downloaded_file, caption=content, parse_mode='HTML')
+        bot.send_audio(
+            CONFESSION,
+            downloaded_file,
+            title=message.audio.title,
+            performer=message.audio.performer,
+            caption=content,
+            parse_mode='HTML')
         reply = "<b>Audio has been sent to the confession! Check the channel</b>"
         bot.send_message(message.from_user.id, reply, parse_mode='HTML')
     pass
--- type/video.py
+++ type/video.py
@@ -9,7 +9,8 @@ def _video():
     def __video(message):
         bot.reply_to(message, "<b>Video Received</b>", parse_mode='HTML')
         content = f"<b>{random.choice(PERSON)} {random.choice(CONTENT)}.</b>"
-        bot.forward_message(CONFESSION, message.from_user.id, message.message_id)
+        bot.send_video(chat_id=CONFESSION, data=message.video.file_id, caption=content, parse_mode="HTML")
+        # bot.forward_message(CONFESSION, message.from_user.id, message.message_id)
         reply = "<b>Video has been sent to the confession! Check the channel</b>"
         bot.reply_to(message, reply, parse_mode='HTML')
     pass
