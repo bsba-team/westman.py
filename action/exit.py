59c8e182d9ea8a17aae7738851d54a847a7892d5
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-08 03:44:34 +0500

    Update exit.py

--- action/exit.py
+++ action/exit.py
@@ -4,5 +4,7 @@ from core import bot
 def _exit():
     @bot.message_handler(commands=['exit'])
     def __exit(message):
+        bot.stop_polling()
+        bot.stop_bot()
         exit(1)
         pass
