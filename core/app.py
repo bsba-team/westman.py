be43d890b41e5f849ffa1f3a521630ac07d1e4d3
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-08 04:36:10 +0500

    Update app.py

--- core/app.py
+++ core/app.py
@@ -12,7 +12,7 @@ def localhost():
     logger.setLevel(logging.DEBUG)
     while True:
         try:
-            bot.infinity_polling(none_stop=True)
+            bot.polling(none_stop=True)
         except ConnectionRefusedError:
             time.sleep(10)
         pass
